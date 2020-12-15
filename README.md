87dada5a2e3fe84b9167bf20ba3c3b281e97abee
JuHyeon Park <33710525+beotborry@users.noreply.github.com>
2020-12-16 00:57:03 +0900

    CNN Part Complete

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # MaskNet
+## 2020F-IntroductionToDeepLearning-Term Project
+
+1. Mobilenet.ipynb
+Keras 안에 구현되어 있는 MobilenetV2를 이용하여 Fine Tuning 하는 코드입니다.
+
+1. utils.py
+Data loader, plotting 함수가 구현되어 있습니다.
---
+++ utils.py
@@ -0,0 +1,45 @@
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+
+import matplotlib.pyplot as plt
+
+def load_data_with_label(path_arr, data_arr, label_arr, img_size, _label):
+    for path in path_arr:
+        try:
+            img = load_img(path, target_size = img_size)
+            img = img_to_array(img)
+            img = preprocess_input(img)
+            data_arr.append(img)
+            label_arr.append(_label)
+        except:
+            print(path)
+
+def training_log_plot(history):
+    accuracy = history.history['accuracy']
+    val_accuracy = history.history['val_accuracy']
+
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    plt.figure(figsize=(8,8))
+    plt.subplot(2, 1, 1)
+    plt.plot(accuracy, label = "TRNG_ACC")
+    plt.plot(val_accuracy, label = "VAL_ACC")
+
+    plt.ylabel("Accuracy")
+    plt.title("Accuracy")
+
+    plt.subplot(2, 1, 2)
+    plt.plot(loss, label = "TRNG_LOSS")
+    plt.plot(val_loss, label = "VAL_LOSS")
+
+    plt.xlabel("Epoch")
+    plt.ylabel("Cross Entropy")
+    plt.title("Loss")
+
+    plt.savefig("./training_log/fig.png")
+    plt.show()
+
+def export_model_summary(model):
+    with open("report.txt", "w") as fh:
+        model.summary(print_fn=lambda x: fh.write(x + '\n'))
